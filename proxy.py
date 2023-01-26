7325082baa36630aa7c38069c488dc0967956713
Katelyn Gigante <clockwork.singularity@gmail.com>
2023-01-26 21:57:46 +1100

    Further proxy improvements

--- proxy.py
+++ proxy.py
@@ -5,17 +5,30 @@ import os
 import socket
 import threading
 from select import select
-from typing import Any, Callable
+from typing import Any, Callable, TypeVar
 
 import attrs
 import mudtelnet
 
 SINGLE_CLIENT = False
 
+KeyType = TypeVar('KeyType')
+
+
+def deep_update(mapping: dict[KeyType, Any], *updating_mappings: dict[KeyType, Any]) -> dict[KeyType, Any]:
+    updated_mapping = mapping.copy()
+    for updating_mapping in updating_mappings:
+        for k, v in updating_mapping.items():
+            if k in updated_mapping and isinstance(updated_mapping[k], dict) and isinstance(v, dict):
+                updated_mapping[k] = deep_update(updated_mapping[k], v)
+            else:
+                updated_mapping[k] = v
+    return updated_mapping
+
 
 @attrs.define(slots=False)
 class Client():
-    fd: socket.socket
+    fd: socket.socket = attrs.field(repr=False)
     addr: tuple[str, int]
     oob_out: tuple[int, int]
     has_gmcp: bool | None = None
@@ -112,7 +125,7 @@ def serve(PipeW: int, pipeToSocketR: int, sock: socket.socket, stop: threading.E
                     changed = s.connection.process_frame(frame, out_buffer, out_events)
                     if changed:
                         print(repr(changed))
-                        s.state.update(changed)
+                        s.state = deep_update(s.state, changed)
                     for e in out_events:
                         if isinstance(e, mudtelnet.TelnetInMessage):
                             if e.data.startswith(b'#$#'):
